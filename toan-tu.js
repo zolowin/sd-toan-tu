5874f15bf33932c19e3c20f3fdea4f69b66003c6
zolowin <57177546+zolowin@users.noreply.github.com>
2019-11-04 09:57:50 +0700

    thêm file

---
+++ toan-tu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt('enter the width');
+inputHeight=prompt('enter the height');
+let width= parseInt(inputWidth);
+let height= parseInt(inputHeight);
+let area=width*height;
+document.write('The area is: '+ area)
